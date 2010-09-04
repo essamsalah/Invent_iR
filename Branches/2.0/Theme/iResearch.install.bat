c12fb1d3b0c78598264799247fc828aa2432e476
essam@ieee.org <essam@ieee.org>
2010-09-04 11:44:24 +0000

    Create a branch for version 2.0

---
+++ Branches/2.0/Theme/iResearch.install.bat
@@ -0,0 +1,9 @@
+SET OFFICE_THEMES=C:\Program Files\Microsoft Office\Document Themes 12
+
+copy ".\iResearch.thmx" "%OFFICE_THEMES%"
+
+copy ".\Theme Colors\iResearch.xml" "%OFFICE_THEMES%\Theme Colors"
+
+copy ".\Theme Fonts\iResearch.xml" "%OFFICE_THEMES%\Theme Fonts"
+
+PAUSE
